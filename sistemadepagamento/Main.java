44dbada0f3ccf8925d99174010cfc4db5f1f8233
isabela <162368740+Mueller07@users.noreply.github.com>
2024-10-23 15:19:47 -0300

    Main.java

--- sistemadepagamento/Main.java
+++ sistemadepagamento/Main.java
@@ -9,5 +9,16 @@ package com.mycompany.sistemadepagamento;
  * @author ISABELADACOSTAMUELLE
  */
 public class Main {
+    public static void main(String[] args){
+        CartaoCredito meuCartao = new CartaoCredito();
+        PayPal meuPayPal = new PayPal();
+        
+        meuCartao.processarPagamento(50.90);
+        meuPayPal.processarPagamento(99.90);
+        
+        System.out.println(meuCartao.obterDetalhes());
+        System.out.println(meuPayPal.obterDetalhes());
+    }
     
 }
+
